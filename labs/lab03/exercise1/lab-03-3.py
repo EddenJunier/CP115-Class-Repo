f4701568ee1d699d92508ad39bb6af57e914f192
EddenJunier <eddenjunierpersonal@gmail.com>
2026-07-08 12:22:22 +0800

    FIQRY COMMIT

--- labs/lab03/exercise1/lab-03-3.py
+++ labs/lab03/exercise1/lab-03-3.py
@@ -1,8 +1,8 @@
 kilobyte = float(input())
 megabyte = kilobyte * 0.001
-gigabyte = kilobyte * 1E-06.0
-terabyte = kilobyte * 1E-09.0
-petabyte = kilobyte * 1E-12.0
+gigabyte = kilobyte * 1E-06
+terabyte = kilobyte * 1E-09
+petabyte = kilobyte * 1E-12
 print(megabyte)
 print(gigabyte)
 print(terabyte)
