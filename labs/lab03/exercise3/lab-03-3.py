d6ff26677c46052bf7332b09ae5c731b970bc19e
EddenJunier <eddenjunierpersonal@gmail.com>
2026-07-15 12:14:15 +0800

    commityhtsg

--- labs/lab03/exercise3/lab-03-3.py
+++ labs/lab03/exercise3/lab-03-3.py
@@ -1,8 +1,8 @@
 kilobyte = float(input())
 megabyte = kilobyte * 0.001
-gigabyte = kilobyte * 1E-06
-terabyte = kilobyte * 1E-09
-petabyte = kilobyte * 1E-12
+gigabyte = kilobyte * 0.000001
+terabyte = kilobyte * 0.000000001
+petabyte = kilobyte * 0.000000000001
 print(megabyte)
 print(gigabyte)
 print(terabyte)
