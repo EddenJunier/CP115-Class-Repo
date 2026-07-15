4fbb460df99a9cd3e599f1c7e7140709d7fa0422
EddenJunier <eddenjunierpersonal@gmail.com>
2026-07-15 12:18:30 +0800

    COMMIT N MUSH

--- labs/lab03/exercise3/lab-03-3.py
+++ labs/lab03/exercise3/lab-03-3.py
@@ -1,8 +1,8 @@
 kilobyte = float(input())
-megabyte = kilobyte * 0.001
-gigabyte = kilobyte * 0.000001
-terabyte = kilobyte * 0.000000001
-petabyte = kilobyte * 0.000000000001
+megabyte = (kilobyte * 0.001)
+gigabyte = (kilobyte * 0.000001)
+terabyte = (kilobyte * 0.000000001)
+petabyte = (kilobyte * 0.000000000001)
 print(megabyte)
 print(gigabyte)
 print(terabyte)
