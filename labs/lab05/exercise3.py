f85fcc5d10c715deecac9432ef831530d15323d5
EddenJunier <eddenjunierpersonal@gmail.com>
2026-08-05 12:01:51 +0800

    piton

---
+++ labs/lab05/exercise3.py
@@ -0,0 +1,13 @@
+#random student selector
+
+import random
+
+students = ["Alice", "Bob", "Charlie", "Diana", "Eve", "Frank", "Grace", "Hannah", "Ian", "Jack","Fiqry"]
+selected_student = random.choice(students)
+print("\n")
+
+print(f"Selected student: {selected_student}")
+print("\n")
+
+print("Sellected stdent will be enrolled into military training for 3 months as a punishment for not attending class on time and not submitting assignments on time. ")
+print("\n")
