dfeadc09ab4097895d50200e2618c02e55038262
EddenJunier <eddenjunierpersonal@gmail.com>
2026-08-12 11:59:49 +0800

    labtest2

---
+++ labs/lab06/exercise1/test/lab-test-2.py
@@ -0,0 +1,25 @@
+# State the required information which is Name and Matric no.
+# Store it in the appropriate variable with appropriate name
+Name = "Ahmad Bin Abu"
+Matric_No = "MS2025123499"
+
+# Calculate the mark to be displayed in the output
+Mark1 = 10
+Mark2 = 10
+full_mark = Mark1 + Mark2
+
+print(f"""
+Name: {Name}\t\t\tMatric.No: {Matric_No}\n
+*\t\t\t*
+**\t\t**
+***\t***
+********
+***\t***
+**\t\t**
+*\t\t\t*
+
+This is my
+\tsecond
+\t\tassignment
+I want 2x10 marks, which is {full_mark} full marks\n
+""")
