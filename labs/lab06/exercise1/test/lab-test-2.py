ebe679b53c3da17cf65b1302296c30f5b82c6599
EddenJunier <eddenjunierpersonal@gmail.com>
2026-08-12 12:06:19 +0800

    assignment222

--- labs/lab06/exercise1/test/lab-test-2.py
+++ labs/lab06/exercise1/test/lab-test-2.py
@@ -8,6 +8,10 @@ Mark1 = 10
 Mark2 = 10
 full_mark = Mark1 + Mark2
 
+
+# Output code
+# Using triple """ to print output in one go
+
 print(f"""
 Name: {Name}\t\t\tMatric.No: {Matric_No}\n
 *\t\t\t*
@@ -17,9 +21,7 @@ Name: {Name}\t\t\tMatric.No: {Matric_No}\n
 ***\t***
 **\t\t**
 *\t\t\t*
-
 This is my
 \tsecond
 \t\tassignment
-I want 2x10 marks, which is {full_mark} full marks\n
-""")
+I want 2x10 marks, which is {full_mark} full marks\n""")
